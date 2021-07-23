99e742408da4ea7ec5876b052af7ef96e48956e1
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-23 20:12:29 -0300

    exercicio 1 - concluido

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,11 +2,41 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            currentPokemon: 0,
+        }
+        this.handleBack = this.handleBack.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+    }
+
+    handleBack() {
+        this.setState(prevState => ({
+            currentPokemon: prevState.currentPokemon ? (prevState.currentPokemon - 1) : this.props.pokemons.length - 1,
+        }));
+    }
+
+    handleNext() {
+        this.setState(prevState => ({
+            currentPokemon: (prevState.currentPokemon + 1) % this.props.pokemons.length,
+        }));
+    }
+
     render() {
+        const pokemon = this.props.pokemons[this.state.currentPokemon];
+
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
-            </div>
+            <>
+                <div className="pokedex">
+                    <Pokemon key={pokemon.id} pokemon={pokemon} />
+                </div>
+                <div className="control">
+                    <button onClick={this.handleBack}>Anterior</button>
+                    <button onClick={this.handleNext}>Próximo</button>
+                </div>
+            </>
         );
     }
 }
